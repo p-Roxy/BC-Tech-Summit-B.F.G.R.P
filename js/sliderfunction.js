7ef3c5476a4cf4774d75359861289d7f2d0020a9
p-Roxy <prom.roxy@gmail.com>
2017-02-06 12:45:34 -0800

    Added selection page portions

--- js/sliderfunction.js
+++ js/sliderfunction.js
@@ -1,10 +1,20 @@
-/** Snap to 0 function for sliders **/
-function touchUp(sliderID) {
-    var x = document.getElementById(sliderID);
-    var returnVal = x.defaultValue;
-    var currentVal = x.value;
+/** Snap to 0 function for sliders
+    @param slider
+        the unique slider obj intended to snap back
+        Sets the value of the given slider back to 0
+ **/
+function snapBack(slider) {
+    var returnVal = slider.defaultValue;
+    var currentVal = slider.value;
 
     if (returnVal != currentVal) {
-        x.value = returnVal;
+        slider.value = returnVal;
     }
+}
+
+/** Saves the value of a slider in a json file
+ *  @param slider
+ */
+function saveVal(slider) {
+
 }
